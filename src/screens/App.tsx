fa218eec9efe29fa68993ae548d4fc9183fae969
losh11 <loshan1212@gmail.com>
2024-09-14 02:15:56 +0100

    rtk + expo-secure-store + persist

---
+++ src/screens/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+interface Props {}
+
+const App: React.FC<Props> = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
